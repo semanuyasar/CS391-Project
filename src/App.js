37485c235f3e45d61b48bfe5e0fd88434bd5e141
Berkin Tan <berkin.tan@ozu.edu.tr>
2023-05-23 17:11:06 +0300

    add item-details page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navbar from './Navbar.js';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App hi</h1>
+    <div className="item-details-page">
+
+      <Navbar />
+
+      <div className='container item-details-body'>
+
+        <div className="row banner">
+          <img src="banner.jpg" alt="Item" />
+        </div>
+        <div className="row item-details">
+          <div className='col item-details-left'>
+            <img src="macbook.jpg" alt="Item" />
+          </div>
+          <div className='col item-details-right'>
+            <h2>Item Name</h2>
+            <h4>Brand</h4>
+            <span>Price</span>
+            <p>Item Description</p>
+            <p>Item Price</p>
+            <button type="button" class="btn btn-success">Add To Card</button>
+          </div>
+        </div>
+        <div className="row item-more-photos">
+
+        </div>
+
+      </div>
     </div>
   );
 }
