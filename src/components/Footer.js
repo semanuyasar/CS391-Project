221825b67440aac03f05af82ccee795b7a71a014
Semanur Yasar <semanur.yasar@ozu.edu.tr>
2023-05-25 22:32:24 +0300

    footer update with icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './Footer.css';
 
 const Footer = () => {
@@ -13,7 +14,7 @@ const Footer = () => {
           </Col>
           <Col md={4}>
             <h5>Contact Us</h5>
-            <p>Email: info@example.com</p>
+            <p>Email: info@everythinghere.com</p>
             <p>Phone: +1 123-456-7890</p>
           </Col>
           <Col md={4}>
